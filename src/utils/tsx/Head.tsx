110665341a3061f7f45513c63f40a79748aead4b
makreevds <makreevds@yandex.ru>
2026-01-03 17:48:49 +0300

    6.5

--- src/utils/tsx/Head.tsx
+++ src/utils/tsx/Head.tsx
@@ -3,8 +3,9 @@ import '../css/Head.css'
 export function Head() {
   return (
     <header className="head">
-      {/* Пустой хедер для создания отступа от верха экрана */}
+      <div className="head-title">
+        WishMe
+      </div>
     </header>
-  );
+  )
 }
-
