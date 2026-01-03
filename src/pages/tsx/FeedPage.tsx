0f799bea957db750a26e1056f4828875874bbc2d
makreevds <makreevds@yandex.ru>
2026-01-03 17:43:07 +0300

    6.4

--- src/pages/tsx/FeedPage.tsx
+++ src/pages/tsx/FeedPage.tsx
@@ -3,7 +3,7 @@ import '../css/FeedPage.css'
 export function FeedPage() {
   return (
     <div className="feed-page">
-      <h1>Друзья</h1>
+      <h1>Новости</h1>
       <p className="placeholder-text">Страница в разработке...</p>
     </div>
   );
